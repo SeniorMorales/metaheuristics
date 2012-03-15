c7e1b59a956f433b03c2c15ff6bb26e54582074d
marcommireles <marcommireles@gmail.com>
2012-03-15 05:26:19 +0000

    Removed unnecessary variables.

--- anneal.py
+++ anneal.py
@@ -3,10 +3,6 @@ import random
 import optimization
 
 state = [random.randrange(100), random.randrange(100)]
-neighborhood = [state]
-
-max_eval = 100
-eval_count_energy = 0
 
 def fitness(state):
   return optimization.de_jong(state[0], state[1])
@@ -19,8 +15,6 @@ def neighbor(state, x_range=1.0, y_range=1.0):
 
 def temperature(value, adjustment=10000):
   return adjustment * value
-
-state = [random.randrange(100), random.randrange(100)]
 
 def annealing(state):
   current = list(state)
